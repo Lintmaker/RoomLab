ff85cc82869a1bebbaa826123e7928913290aefe
BT_1N3_32 <lpan7932@bths.edu>
2018-11-28 14:49:11 -0500

    Initial Commit

---
+++ src/Rooms/DeathRoom.java
@@ -0,0 +1,44 @@
+package Rooms;
+import Game.Runner;
+import People.Person;
+import java.util.Scanner;
+public class DeathRoom extends Room {
+    public DeathRoom(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void enterRoom(Person YoureDead) {
+        String response = "";
+        occupant = YoureDead;
+        YoureDead.setxLoc(this.xLoc);
+        YoureDead.setyLoc(this.yLoc);
+        if (xLoc < 2) {
+            System.out.println("You see a lit match fall onto a Cannon");
+            System.out.println("3");
+            System.out.println("2");
+            System.out.println("       __\n" +
+                    "                 /  \\\n" +
+                    "           .-.  |    |\n" +
+                    "   *    _.-'  \\  \\__/\n" +
+                    "    \\.-'       \\\n" +
+                    "   /          _/\n" +
+                    "  |      _  /\"\n" +
+                    "  |     /_\\'\n" +
+                    "   \\    \\_/\n" +
+                    "    \"\"\"\"");
+            System.out.println("Wah Wah try again");
+            Runner.gameOff();
+        }
+        if (xLoc ==3)
+        {
+           Scanner Response = new Scanner (System.in);
+            System.out.println("You see a red drink. Do you drink it?");
+            if (response.equals("yes"))
+            {
+                System.out.println();
+            }
+        }
+        Runner.gameOff();
+    }
+}
